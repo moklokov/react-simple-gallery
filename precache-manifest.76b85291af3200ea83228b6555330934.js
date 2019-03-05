239e20da0c1aea34a3fa299a73d55de506ebbe53
Konstantin <moklokov.kostya@gmail.com>
2019-03-05 23:20:48 +0200

    Updates

---
+++ precache-manifest.76b85291af3200ea83228b6555330934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8f7a0ff8608beae0159",
+    "url": "/react-simple-gallery/static/js/runtime~main.a8f7a0ff.js"
+  },
+  {
+    "revision": "8460b9d564f35cf00d94",
+    "url": "/react-simple-gallery/static/js/main.8460b9d5.chunk.js"
+  },
+  {
+    "revision": "78921f61975c49d69b4e",
+    "url": "/react-simple-gallery/static/js/2.78921f61.chunk.js"
+  },
+  {
+    "revision": "8460b9d564f35cf00d94",
+    "url": "/react-simple-gallery/static/css/main.d10368aa.chunk.css"
+  },
+  {
+    "revision": "acbd97055501c7fafc4227d3cfed1820",
+    "url": "/react-simple-gallery/index.html"
+  }
+];
